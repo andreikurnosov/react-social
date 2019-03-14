282dc4b354037a0abb7fd0b48d2af2e92fe30077
nixdie <nixdie@yandex.ru>
2019-03-14 11:12:05 +0700

    creating jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+
 //Load User modal
 const User = require('../../models/User');
 
@@ -45,33 +48,44 @@ router.post('/register', (req, res) => {
   });
 });
 
-
 // @route     GET api/users/login
 // @desc      Login User / Returning JWT Token
 // @access    Public
-router.post('/login', (req,res) => {
+router.post('/login', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
   // Find user by email
-  User.findOne({email})
-    .then(user => {
-      // Check for user
-      if(!user) {
-        return res.status(404).json({email: 'User not found'})
-      }
+  User.findOne({ email }).then((user) => {
+    // Check for user
+    if (!user) {
+      return res.status(404).json({ email: 'User not found' });
+    }
 
-      // Check password
-      bcrypt.compare(password, user.password)
-        .then(isMatch => {
-          if(isMatch) {
-            res.json({msg: 'Success'});
-          } else {
-            return res.status(400).json({password: 'Password incorrect'});
+    // Check password
+    bcrypt.compare(password, user.password).then((isMatch) => {
+      if (isMatch) {
+        // User Matched
+
+        const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT Payload
+
+        // Sign Token
+        jwt.sign(
+          payload,
+          keys.secretOrkey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: 'Bearer ' + token
+            });
           }
-        })
-    })
-})
-
+        );
+      } else {
+        return res.status(400).json({ password: 'Password incorrect' });
+      }
+    });
+  });
+});
 
 module.exports = router;
