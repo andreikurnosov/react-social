826533a5edc821e5aa7a82886c03a9a97d0d3275
nixdie <nixdie@yandex.ru>
2019-03-14 22:14:37 +0700

    profile field Validations

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,10 +3,15 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+
+// Load Validation
+const validateProfileInput = require('../../validation/profile');
+
 // Load Profile Model
 const Profile = require('../../models/Profile');
 // Load User Model
 const User = require('../../models/User');
+
 
 // @route   GET api/profile/test
 // @desc    Tests profile route
@@ -21,7 +26,9 @@ router.get(
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
     const errors = {};
+
     Profile.findOne({ user: req.user.id })
+      .populate('user', ['name', 'avatar'])
       .then((profile) => {
         if (!profile) {
           errors.noprofile = 'There is no profile for this user';
@@ -40,6 +47,14 @@ router.post(
   '/',
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
+    const { errors, isValid} = validateProfileInput(req.body);
+
+    // Check validation
+    if(!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors)
+    }
+
     // Get fields
     const profileFields = {};
     profileFields.user = req.user.id;
