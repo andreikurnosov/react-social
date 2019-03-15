9708cbb488d9a7e94516c2ab90648a3e0e49909a
nixdie <nixdie@yandex.ru>
2019-03-15 15:08:15 +0700

    basic layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       <h1>My React Appppppppppp</h1>
-      </div>
-    );
-  }
-}
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
 
-export default App;
+
+const app = (props) => {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Landing />
+      <Footer />
+    </div>
+  );
+};
+
+export default app;
---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const footer = (props) => {
+  return (
+    <footer className="bg-dark text-white mt-5 p-4 text-center" >
+      Copyright &copy; { new Date().getFullYear()} React Social
+
+    </footer>
+  )
+}
+
+export default footer;
