992263285c79fe028d8695f9640b67c53565e21f
nixdie <nixdie@yandex.ru>
2019-03-18 14:23:08 +0700

    react router setup

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const register = (props) => {
+  return (
+    <div>
+      <h1>Register!!!!</h1>
+    </div>
+  );
+};
+export default register;
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const landing = (props) => {
   return (
@@ -7,20 +8,25 @@ const landing = (props) => {
         <div className="container">
           <div className="row">
             <div className="col-md-12 text-center">
-              <h1 className="display-3 mb-4">Developer Connector
-              </h1>
-              <p className="lead"> Create a developer profile/portfolio, share posts and get help from other developers</p>
+              <h1 className="display-3 mb-4">Developer Connector</h1>
+              <p className="lead">
+                {' '}
+                Create a developer profile/portfolio, share posts and get help
+                from other developers
+              </p>
               <hr />
-              <a href="register.html" className="btn btn-lg btn-info mr-2">Sign Up</a>
-              <a href="login.html" className="btn btn-lg btn-light">Login</a>
+              <Link to="/register" className="btn btn-lg btn-info mr-2">
+                Sign Up
+              </Link>
+              <Link to="/login" className="btn btn-lg btn-light">
+                Login
+              </Link>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
-
+  );
+};
 
 export default landing;
-
